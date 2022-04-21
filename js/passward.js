bf00c568e282e4901d13616bddd5b8753d5d9b13
Anastasiia Pogontseva <anastasiia_pogontseva@epam.com>
2022-04-21 07:33:02 +0300

    New File with Password-Task

---
+++ js/passward.js
@@ -0,0 +1,6 @@
+let passward = prompt ('Enter Passward');
+if (passward.length > 8 && passward.length < 20 ) {
+    console.log ("Correct Passward");
+} else {
+    console.log ("INCorrect Passward");
+}
